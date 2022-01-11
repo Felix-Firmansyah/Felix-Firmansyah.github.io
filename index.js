6d132247b03a1063e14e89b23909a2394d201e10
Felix Firmansyah <97281380+Felix-Firmansyah@users.noreply.github.com>
2022-01-11 10:49:31 +0700

    Update Js

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ for (i; i < all_button.length; i++) {
 		sounds.play();
 	});
 }
+
+/*==================== FORM FUNCITON ====================*/ 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyrnF-v9mgKQBuVQ4KInPE0ECi5YeNTeNyFH45ysVZy-ZLjYKlPHzB83oCiX6rLy2tUrg/exec'
+const form = document.forms['contact-my-website']
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const btnDAlert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+// ketika tombol submit di klik 
+// tampilkan tombol loading , hilangkan tombol kirim
+btnLoading.classList.toggle("d-none");
+btnKirim.classList.toggle("d-none");
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => {
+    // tampilkan tombol kirim , hilangkan tombol loading
+    btnLoading.classList.toggle("d-none");
+    btnKirim.classList.toggle("d-none");
+    // tampilkan alert
+    btnDAlert.classList.toggle('d-none');
+    // reset tulisan input form nya
+    form.reset()
+    console.log('Success!', response)})
+  .catch(error => console.error('Error!', error.message))
+})
+$('.js-scroll-trigger').click(function() {
+  $('.navbar-collapse').collapse('hide');
+});
